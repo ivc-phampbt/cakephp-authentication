43693f92226d1dad9a9534327a5df0b3889c8d1a
pbqtai <pbqtai@vn.isb.co.jp>
2024-03-29 08:45:15 +0700

    update web login screen.

--- app/src/Controller/UsersController.php
+++ app/src/Controller/UsersController.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 use App\Form\LoginForm;
-use Authentication\PasswordHasher\DefaultPasswordHasher;
 use Cake\Http\Exception\NotFoundException;
 use Firebase\JWT\JWT;
+use Cake\Event\EventInterface;
 
 /**
  * Users Controller
@@ -21,44 +21,10 @@ class UsersController extends AppController
         parent::initialize();
     }
 
-    public function beforeFilter(\Cake\Event\EventInterface $event)
+    public function beforeFilter(EventInterface $event)
     {
         parent::beforeFilter($event);
-        $this->Authentication->allowUnauthenticated(['login', 'webLogin']);
-    }
-
-    public function login()
-    {
-        if ($this->request->is('post')) {
-            $result = $this->Authentication->getResult();
-            if ($result->isValid()) {
-
-                $privateKey = file_get_contents(CONFIG . 'jwt.key');
-                $user = $result->getData();
-                $payload = [
-                    'iss' => 'myapp',
-                    'sub' => $user->id,
-                    'exp' => time() + 3600, // 1 hour
-                ];
-
-                $json = [
-                    'token' => JWT::encode($payload, $privateKey, 'RS256'),
-                ];
-            } else {
-                $this->response = $this->response->withStatus(401);
-                $json = [
-                    "message" => __('The Username or Password is Incorrect')
-                ];
-            }
-
-            $this->RequestHandler->renderAs($this, 'json');
-            $this->response->withType('application/json');
-
-            $this->set(['json' => $json]);
-            $this->viewBuilder()->setOption('serialize', 'json');
-        } else {
-            throw new NotFoundException();
-        }
+        $this->Authentication->allowUnauthenticated(['webLogin']);
     }
 
     public function webLogin()
